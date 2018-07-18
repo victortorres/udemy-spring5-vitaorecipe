a36a670d8e8ec3bd2d3cdf90cc5955e933e1e2da
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-19 00:56:26 +0100

    primeiro commit

---
+++ src/main/java/br/com/vtorres/udemy/vitaorecipe/VitaorecipeApplication.java
@@ -0,0 +1,12 @@
+package br.com.vtorres.udemy.vitaorecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaorecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaorecipeApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/vtorres/udemy/vitaorecipe/VitaorecipeApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vtorres.udemy.vitaorecipe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VitaorecipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
